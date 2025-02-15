461ae67bbab2bee8dee9d6540105cd2afb13f615
jackellingwood <ellingwoodjack@gmail.com>
2025-02-15 10:16:30 -0500

    Added Patient.java

---
+++ src/Patient.java
@@ -0,0 +1,9 @@
+public class Patient {
+    private int _id;
+    private double _caffeine;
+
+    public Patient(int id, double caffeine) {
+        _id = id;
+        _caffeine = caffeine;
+    }
+}
